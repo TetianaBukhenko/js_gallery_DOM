ce5af0f1adca59cfa50383340c88909e26419512
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-13 03:28:59 +0300

    make build

---
+++ index.665a1360.js
@@ -0,0 +1,2 @@
+document.querySelector("#thumbs").addEventListener("click",function(e){var t,r,c=e.target.closest("a");e.preventDefault(),c&&(t=c.href,(r=document.querySelector("#largeImg")).removeAttribute("src"),r.setAttribute("src",t))});
+//# sourceMappingURL=index.665a1360.js.map
